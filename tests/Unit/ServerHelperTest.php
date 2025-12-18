0983e50eccb22ae21f16e41f01f41d250bbaed7b
Damian Wilusz <kontakt@damianwilusz.eu>
2025-12-18 18:49:00 +0100

    tests: cleanup server helper tests

--- tests/Unit/ServerHelperTest.php
+++ tests/Unit/ServerHelperTest.php
@@ -7,14 +7,6 @@ use WHMCS\Module\Server\PanelAlpha\Helper;
 
 class ServerHelperTest extends TestCase
 {
-    public function testGenerateRandomString()
-    {
-        $length = 20;
-        $string = Helper::generateRandomString($length);
-        $this->assertEquals($length, strlen($string));
-        $this->assertMatchesRegularExpression('/^[a-zA-Z0-9]+$/', $string);
-    }
-
     public function testGenerateSecurePassword()
     {
         $length = 16;
