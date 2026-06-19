05f06d32849a3ef0c563beb01b78ead67609d216
Sebastian Kostecki <sebastian.kostecki@panelalpha.com>
2026-06-19 09:32:24 +0200

    fix(sso): improve link style

--- modules/servers/panelalpha/hooks.php
+++ modules/servers/panelalpha/hooks.php
@@ -47,7 +47,7 @@ add_hook('ClientAreaSecondaryNavbar', 1, function (MenuItem $secondaryNavbar) {
 
         if ($panelAlphaFirstService->configoption5 === 'on') {
             $secondaryNavbar->addChild('panelalpha_sso_link', array(
-                'label' => '<span style="margin-right: 12px; color: #5bc0de;" onMouseOver="this.style.textDecoration=\'underline\'"  onMouseOut="this.style.textDecoration=\'none\'">' . $MGLANG['ca']['general']['panelalpha']['sso_link'] . ' <i class="fas fa-external-link"></i></span>',
+                'label' => '<span style="margin-left: 12px; margin-right: 12px; color: #5bc0de;" onMouseOver="this.style.textDecoration=\'underline\'"  onMouseOut="this.style.textDecoration=\'none\'">' . $MGLANG['ca']['general']['panelalpha']['sso_link'] . ' <i class="fas fa-external-link"></i></span>',
                 'order' => 1,
                 'uri' => $CONFIG['SystemURL'] . '/clientarea.php?action=productdetails&sso=yes&id=' . $panelAlphaFirstService->id,
             ));
